5980b2fb640fa781325bb0f2fef09f0f8cab4f1b
Arjun Palani <arjun.palani@sada.com>
2023-12-31 21:09:49 +0530

    Models created in all 4 apps

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+from customers.models import Customer
+from products.models import Product
 # Create your models here.
+# an order is an order status 
+class Order(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),DELETE('Delete'))
+    CART_STAGE=0
+    ORDER_CONFIRMED=1
+    ORDER_PROCESSED=2
+    ORDER_DELIVERED=3
+    ORDER_REJECTED=4
+    STATUS_CHOICES=( ((ORDER_CONFIRMED,"ORDER_CONFIRMED"),
+                      (ORDER_PROCESSED,"ORDER_PROCESSED"),
+                      (ORDER_DELIVERED,"ORDER_DELIVERED")),)
+    order_status= models.IntegerField(choices=STATUS_CHOICES,default=CART_STAGE)
+
+
+
+
+    # a single customer can have multiple orders (for example, if they start a new order after completing an old one without checking out).
+    owner=models.ForeignKey(Customer,on_delete=models.SET_NULL,related_name='orders')
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+# an order can have multiple OrderedItems
+class OrderedItem(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),DELETE('Delete'))
+    #a single Product may be present as many OrderedItems (ie; an order can have multiple copies of same product as OrderedItem)
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,related_name='added_carts')
+    quantity = models.IntegerField(default=1)
+    #a single order contains multiple orderedItems
+    owner = models.ForeignKey(Order,on_delete=models.CASCADE,related_name='added_items')
+
+    delete_status=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
