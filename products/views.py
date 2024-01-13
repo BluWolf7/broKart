bd1195ebd49567f4ee8784c72ec659629d5d4d35
Arjun Palani <arjun.palani@sada.com>
2024-01-13 16:38:01 +0530

    Adding Layouts  : 3

--- products/views.py
+++ products/views.py
@@ -3,7 +3,13 @@ from . models import Product
 from django.core.paginator import Paginator
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    featured_products = Product.objects.order_by('priority')[:4]
+    latest_products = Product.objects.order_by('-id')[:4]
+    context = {
+           'featured_products':featured_products,
+           'latest_products': latest_products
+           }
+    return render(request,'index.html',context)
 
 def list_products(request):
         """
@@ -18,11 +24,13 @@ def list_products(request):
         page=1
         if request.GET:
                page=request.GET.get('page',1)
-        product_list = Product.objects.all()
-        product_paginator = Paginator(product_list,4)
+        product_list = Product.objects.order_by('-priority')
+        product_paginator = Paginator(product_list,2)
         product_list = product_paginator.get_page(page)
         context ={'products': product_list}
         return render(request,'products.html',context)
 
-def detail_product(request):
-        return render(request,'product_detail.html')
+def detail_product(request,pk):
+        product = Product.objects.get(pk=pk)
+        context = {'product':product}
+        return render(request,'product_detail.html',context)
